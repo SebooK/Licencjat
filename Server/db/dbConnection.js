5142571244727d650aaec318cb7e554031f73c35
SeeboK <seba9781@gmail.com>
2021-01-22 23:51:16 +0100

    configured Sequelize database connection for both environments

--- Server/db/dbConnection.js
+++ Server/db/dbConnection.js
@@ -1,29 +1,43 @@
-import {readFile} from "fs/promises";
-import {Sequelize} from "sequelize";
+import { readFileSync } from "fs";
+import { Sequelize } from "sequelize";
+import winston from "../logger/logger";
 
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = JSON.parse(await readFile('./config/config.json', (err, data) => {
+const env = process.env.NODE_ENV || "development";
+const dbConfig = JSON.parse(
+  readFileSync("../Server/src/config/config.json", (err, data) => {
     if (err) throw err;
-}));
+    winston.info(data);
+  })
+);
 
-const {development} = dbConfig;
-const {production} = dbConfig;
+const { development } = dbConfig;
+const { production } = dbConfig;
 let sequelize;
-if (env === 'development') {
-    sequelize = new Sequelize(development.database, development.username, development.password, {
-        host: development.host,
-        dialect: development.dialect,
-        port: development.port
-    });
+if (env === "development") {
+  sequelize = new Sequelize(
+    development.database,
+    development.username,
+    development.password,
+    {
+      host: development.host,
+      dialect: development.dialect,
+      port: development.port,
+    }
+  );
 } else {
-    sequelize = new Sequelize(production.database, production.username, production.password, {
-        host: production.host,
-        dialect: production.dialect,
-        port: production.port
-    });
+  sequelize = new Sequelize(
+    production.database,
+    production.username,
+    production.password,
+    {
+      host: production.host,
+      dialect: production.dialect,
+      port: production.port,
+    }
+  );
 }
 sequelize
-    .authenticate()
-    .then(() => console.log('Connection has been established successfully'))
-    .catch(err => console.error('Unable to connect to the database:', err));
-export {sequelize}
+  .authenticate()
+  .then(() => winston.info("Connection has been established successfully"))
+  .catch((err) => winston.error("Unable to connect to the database:", err));
+export default sequelize;
